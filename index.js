9630532f7e77418ac826e65c08561b95555c7c6d
Snehal <snehalabhale01@gmail.com>
2023-09-03 15:21:12 -0700

    For andwhile lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const array=["Guadalupe", "Ollie", "Aki"];
+let str="birthday";
+let newArr=[];
+function writeCards(array,str)
+{
+    for(let i=0;i<array.length;i++)
+    {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${str} gift!`);
+    }
+    return newArr;
+}
+
+
+function countDown(n) {
+    while (n>= 0) {
+      console.log(n--);
+    }
+  }
+  countDown(10);
